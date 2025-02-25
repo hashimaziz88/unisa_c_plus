02f5a2423fd1c718058309c4083015f2aecf099d
Hashim Aziz Muhammad <hashimazizm@gmail.com>
2025-02-25 14:33:32 +0200

    add

---
+++ COS1511/New folder/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+// TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+int main() {
+    // TIP Press <shortcut actionId="RenameElement"/> when your caret is at the
+    // <b>lang</b> variable name to see how CLion can help you rename it.
+    int age;
+    std::cout << "Enter your age: ";
+    std::cin >> age;
+    std::cout << age << " a great age to be!"<<std::endl;
+
+    return 0;
+}
+
+// TIP See CLion help at <a
+// href="https://www.jetbrains.com/help/clion/">jetbrains.com/help/clion/</a>.
+//  Also, you can try interactive lessons for CLion by selecting
+//  'Help | Learn IDE Features' from the main menu.
