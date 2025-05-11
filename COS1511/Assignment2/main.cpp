f24db6b744eae6c17ae070b5b57cb694c976bede
Hashim Aziz Muhammad <hashimazizm@gmail.com>
2025-05-11 15:57:58 +0200

    Add main.cpp q6

--- COS1511/Assignment2/main.cpp
+++ COS1511/Assignment2/main.cpp
@@ -2,44 +2,36 @@
 #include <vector>
 #include <string>
 using namespace std;
-
-int getScore() {
-    int score;
-    do {
-        cout << "Enter a test score: ";
-        cin >> score;
-        if (score < 0 || score > 100) {
-            cout << "Invalid score. Please enter a score between 0 and 100." << endl;
-            }
-    } while (score < 0 || score > 100);
-    return score;
+void getData(int &height, int &width, int &length) {
+    cout << "Enter the height of the room: ";
+    cin >> height;
+    cout << "Enter the width of the room: ";
+    cin >> width;
+    cout << "Enter the length of the room: ";
+    cin >> length;
 }
-int findLowest(const vector<int>& scores) {
-    return *min_element(scores.begin(), scores.end());
+int calculateVolume(int theHeight, int theWidth, int theLength) {
+    return theHeight * theWidth * theLength;
 }
-float calcAverage(const vector<int>& scores) {
-    int lowest_score = findLowest(scores);
-    int total = 0;
-    for (int score : scores) {
-        if (score != lowest_score) {
-            total += score;
-        }
-    }
-    return static_cast<float>(total) / 4;
-}
-void displayOutput(float average) {
-    cout.setf(ios::fixed);
-    cout.precision(2);
-    cout << "After dropping the lowest test score, the test average is " <<
-    fixed << average << endl;
+void displayOutput(int height, int width, int length, int volume) {
+    cout << "The volume of a room with height " << height << ", width " <<
+    width << ", and length " << length << " is " << volume << ".\n";
+    // Determine the size of the room
+    string size;
+    if (volume < 100)
+        size = "Small";
+    else if (volume >= 100 && volume <= 500)
+        size = "Medium";
+    else
+        size = "Large";
+    cout << "Size: " << size << endl;
 }
 int main() {
-    vector<int> scores;
     for (int i = 0; i < 5; ++i) {
-        int score = getScore();
-        scores.push_back(score);
+        int theHeight, theWidth, theLength;
+        getData(theHeight, theWidth, theLength);
+        int volume = calculateVolume(theHeight, theWidth, theLength);
+        displayOutput(theHeight, theWidth, theLength, volume);
     }
-    float average = calcAverage(scores);
-    displayOutput(average);
     return 0;
 }
