500a291158fc6f6807b863743c1818f1fb39b994
Hashim Muhammad <hashimazizm@gmail.com>
2025-06-22 14:10:18 +0200

    Add main.cpp q5.1

--- COS1511/Assignment2/main.cpp
+++ COS1511/Assignment2/main.cpp
@@ -1,36 +1,114 @@
-#include <iostream>
-#include <vector>
-#include <string>
+#include <iostream> // for screen/keyboard i/o
+#include <fstream>  // for file
+#include <cstdlib>  // for exit
 using namespace std;
-void getData(int &height, int &width, int &length) {
-    cout << "Enter the height of the room: ";
-    cin >> height;
-    cout << "Enter the width of the room: ";
-    cin >> width;
-    cout << "Enter the length of the room: ";
-    cin >> length;
-}
-int calculateVolume(int theHeight, int theWidth, int theLength) {
-    return theHeight * theWidth * theLength;
-}
-void displayOutput(int height, int width, int length, int volume) {
-    cout << "The volume of a room with height " << height << ", width " <<
-    width << ", and length " << length << " is " << volume << ".\n";
-    // Determine the size of the room
-    string size;
-    if (volume < 100)
-        size = "Small";
-    else if (volume >= 100 && volume <= 500)
-        size = "Medium";
-    else
-        size = "Large";
-}
-int main() {
-    for (int i = 0; i < 5; ++i) {
-        int theHeight, theWidth, theLength;
-        getData(theHeight, theWidth, theLength);
-        int volume = calculateVolume(theHeight, theWidth, theLength);
-        displayOutput(theHeight, theWidth, theLength, volume);
+
+// Precondition:
+// The input file is a text file.
+// The input file has been opened.
+//
+// Postcondition:
+// The output file is a text file.
+// The output file has been opened.
+// Output file will be similar to input file except for all numbers 0 to 7
+// that will be replaced with the character as specified in the question
+
+void checkFile(ifstream& infile)
+{
+    char ch;
+    infile.get(ch);
+    while (!infile.eof())
+    {
+        cout << ch;
+        infile.get(ch);
     }
+}
+
+int main()
+{
+    char convert[8] = {'s','g','o','y','v','n','f','j'};
+    ifstream infile;
+    ofstream outfile;
+    ifstream indisplay;
+    ifstream outdisplay;
+    string inName, outName;
+
+    cout << endl
+         << "Enter the input file name. "
+         << endl;
+    cin >> inName;
+
+    infile.open(inName.c_str());
+    if (infile.fail())
+    {
+        cout << "Cannot open file " << inName << " Aborting!" << endl;
+        exit(1);
+    }
+
+    cout << endl << "Enter the output file name. " << endl
+         << "WARNING: ANY EXISTING FILE WITH THIS NAME WILL"
+         << " BE ERASED." << endl;
+    cin >> outName;
+
+    outfile.open(outName.c_str());
+    if (outfile.fail())
+    {
+        cout << "Cannot open file " << outName << " Aborting!" << endl;
+        exit(1);
+    }
+
+    cout << endl;
+    char ch;
+    int index = 0;
+
+    infile.get(ch);
+    while (!infile.eof())
+    {
+        if (ch >= '0' && ch < '8')
+        {
+            index = int(ch - '0'); // This gives us the numeric version
+                                   // (i.e. 0 – 7 of the characters '0' - '7').
+            outfile << convert[index];
+        }
+        else
+            outfile << ch;
+
+        infile.get(ch);
+    } // end while !infile.eof
+
+    infile.close();
+    outfile.close();
+
+    // This part was not required, but it is always a good idea to read the file
+    // that was created to make sure it is correct.
+    // We first read the original input file, and display its content
+
+    indisplay.open(inName.c_str());
+    if (indisplay.fail())
+    {
+        cout << "Cannot open file " << inName << " Aborting!" << endl;
+        exit(1);
+    }
+
+    cout << endl << "The contents of the input file is : " << endl << endl;
+    checkFile(indisplay);
+    indisplay.close();
+
+    // Now we read the file that was created as an output file and display the
+    // content
+
+    outdisplay.open(outName.c_str());
+    if (outdisplay.fail())
+    {
+        cout << "Cannot open file " << outName << " Aborting!" << endl;
+        exit(1);
+    }
+
+    cout << endl << endl << "The contents of the output file is : "
+         << endl << endl;
+    checkFile(outdisplay);
+    outdisplay.close();
+
+    cout << endl;
     return 0;
 }
